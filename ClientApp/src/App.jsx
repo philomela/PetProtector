8e6eb279435c3cfe869da3d1acac704506d53eb9
philomela <romaphilomela@yandex.ru>
2023-10-24 14:18:16 +0300

    main. Added front pages

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Questionnaire from './pages/Questionnaire/Questionnaire'
+import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/questionnaire" element={<Questionnaire/>}/>
+                <Route path="/home" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
             </Routes>
         </Router>
--- ClientApp/src/pages/Login/Login.jsx
+++ ClientApp/src/pages/Login/Login.jsx
@@ -1,77 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const LoginForm = () => {
-  const [email, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    // Отправка запроса на сервер для аутентификации
-    try {
-      const response = await fetch('https://localhost:5001/api/account/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        debugger;
-        console.log(data);
-        const token = data.token;
-
-        // Сохранение токена в локальном хранилище
-        localStorage.setItem('token', token);
-
-        // Переход на предыдущую страницу
-        window.history.back();
-      } else {
-        setError('Неверные учетные данные');
-      }
-    } catch (error) {
-      setError('Ошибка при отправке запроса');
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   return (
     <div>
-      <h1>Вход</h1>
-      {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="email">Имя пользователя:</label>
+          <label htmlFor="email">Email:</label>
           <input
-            type="text"
-            id="email"
+            name="email"
+            type="email"
             value={email}
-            onChange={handleUsernameChange}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div>
-          <label htmlFor="password">Пароль:</label>
+          <label htmlFor="password">Password:</label>
           <input
+            name="password"
             type="password"
-            id="password"
             value={password}
-            onChange={handlePasswordChange}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button type="submit">Войти</button>
+        <button>Sign In</button>
       </form>
     </div>
   );
 };
 
-export default LoginForm;
+export default Login;
