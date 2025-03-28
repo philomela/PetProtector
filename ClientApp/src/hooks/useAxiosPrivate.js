0b051431ee0e993e1ee6e5fa92301fef1008b9ab
Roman Solovyev <roman.solovyev@sravni.ru>
2025-03-28 23:25:03 +0300

    main. Fix useAxiosPrivare

--- ClientApp/src/hooks/useAxiosPrivate.js
+++ ClientApp/src/hooks/useAxiosPrivate.js
@@ -1,46 +1,49 @@
-import {axiosPrivate} from '../api/axios'
-import {useEffect, useState} from 'react'
-import useRefreshToken from './useRefreshToken'
-import useAuth from './useAuth'
+import { axiosPrivate } from "../api/axios";
+import { useEffect, useState } from "react";
+import useRefreshToken from "./useRefreshToken";
+import useAuth from "./useAuth";
 
 const useAxiosPrivate = () => {
-    const refresh = useRefreshToken();
-    const { auth } = useAuth();
-    const [errorMessage, setErrorMessage] = useState("");
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+  const [errorMessage, setErrorMessage] = useState("");
 
-    useEffect(() => {
-        const requestIntercept = axiosPrivate.interceptors.request.use(
-            config => {
-                if (!config.headers['Authorization']) {
-                    config.headers['Authorization'] = `Bearer ${auth?.accessToken}`;
-                }
-                return config;
-            }, (error) => Promise.reject(error)
-        );
+  useEffect(() => {
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      (config) => {
+        if (!config.headers["Authorization"]) {
+          config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
 
-        const responseIntercept = axiosPrivate.interceptors.response.use(
-            response => response,
-            async (error) => {
-                const prevRequest = error?.config;
-                if (error?.response?.status === 401 && !prevRequest?.sent) {
-                    prevRequest.sent = true;
-                    const newAccessToken = await refresh(auth.accessToken);
-                    prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-                    return axiosPrivate(prevRequest);
-                } else if (error?.response?.status === 500 || error != null) {
-                    setErrorMessage("Что-то пошло не так. Попробуйте снова или напишите нам в поддержку");
-                }
-                return Promise.reject(error);
-            }
-        );
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const prevRequest = error?.config;
+        if (error?.response?.status === 401 && !prevRequest?.sent) {
+          prevRequest.sent = true;
+          const newAccessToken = await refresh(auth.accessToken);
+          prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+          return axiosPrivate(prevRequest);
+        } else if (error?.response?.status === 500 || error != null) {
+          setErrorMessage(
+            "Что-то пошло не так. Попробуйте снова или напишите нам в поддержку"
+          );
+        }
+        return Promise.reject(error);
+      }
+    );
 
-        return () => {
-            axiosPrivate.interceptors.request.eject(requestIntercept);
-            axiosPrivate.interceptors.response.eject(responseIntercept);
-        };
-    }, [auth, refresh]);
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept);
+      axiosPrivate.interceptors.response.eject(responseIntercept);
+    };
+  }, [auth, refresh]);
 
-    return { axiosPrivate, errorMessage, setErrorMessage };
+  return { axiosPrivate, errorMessage, setErrorMessage };
 };
 
 export default useAxiosPrivate;
